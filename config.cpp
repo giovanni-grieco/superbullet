0479b08a6d5c563e6ae3772383f658748ca83b07
Giovanni Pio Grieco <giovi.5057@gmail.com>
2025-08-29 16:18:37 +0200

    Risolti bug

--- config.cpp
+++ config.cpp
@@ -6,8 +6,7 @@ class CfgPatches
         weapons[] = {};
         requiredVersion = 0.1;
         requiredAddons[] = {"A3_Weapons_F"};
-        magazines[] = {"Super_30Rnd_556_AP"};
-        ammo[] = {"B_556x45_Super"};
+        magazines[] = {"30Rnd_556x45_Super"};
     };
 };
 
@@ -33,22 +32,12 @@ class CfgMagazines
         ammo = "B_556x45_Super";
         descriptionShort = "Super Armor-Piercing Rounds";
     };
-
-    /*class 30Rnd_556x45_Super_Emag : 30Rnd_556x45_Stanag
-    {
-        author = "Henderson";
-        displayName = "5.56mm 30Rnd Super EMAG";
-        ammo = "B_556x45_Super";
-        count = 30;
-        initSpeed = 930;
-        descriptionShort = "Custom Armor-Piercing Rounds";
-    };*/
 };
 
 class CfgMagazineWells
 {
     class STANAG_556x45
     {
-        MyMags[] = {"30Rnd_556x45_Super"};
+        Superbullet_Mags[] = {"30Rnd_556x45_Super"};
     };
 };
